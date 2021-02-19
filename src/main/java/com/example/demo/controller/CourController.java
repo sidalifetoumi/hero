eb3d5938af81e52fd38cc7a4884b8527cb22b8ab
sidali fetoumi <sid4a3li@gmail.com>
2021-02-20 00:03:38 +0100

    cors

--- src/main/java/com/example/demo/controller/CourController.java
+++ src/main/java/com/example/demo/controller/CourController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","https://romantic-minsky-4e27dc.netlify.app"})
 @RestController
 @RequestMapping("/cour")
 public class CourController {
